ee4cc9c004a0b3c485b6255f044f292b6439be26
Yury93 <72685333+Yury93@users.noreply.github.com>
2022-02-01 14:13:45 +0300

    2

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -6,8 +6,17 @@ using UnityEngine.SceneManagement;
 public class SceneLoader : SingletonBase<SceneLoader>
 {
     [SerializeField] private int numberScene;
+    [SerializeField] private int numSceneSlotMachine;
     public void LoadScene()
     {
-        SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + numberScene);
+        SceneManager.LoadSceneAsync(numberScene );
+    }
+    public void SelectLoadTape()
+    {
+        SceneManager.LoadSceneAsync(numberScene );
+    }
+    public void SelectLoadSlotMachine()
+    {
+        SceneManager.LoadSceneAsync(numSceneSlotMachine);
     }
 }
